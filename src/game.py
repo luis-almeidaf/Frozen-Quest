1a49b3aa8b53e62165acf5bbdce9753f59f2b8ce
Luis <luis.almeida.faria@gmail.com>
2025-08-10 17:56:30 -0300

    Add initial game and menu configurations

---
+++ src/game.py
@@ -0,0 +1,15 @@
+import pygame
+
+from src.consts import WIN_HEIGHT, WIN_WIDTH
+from src.menu import Menu
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
+
+    def run(self):
+        while True:
+            menu = Menu(self.window)
+            menu.run()
