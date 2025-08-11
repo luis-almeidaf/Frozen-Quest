ba43d8d262c11279f978f743d786c8c5ce5951da
Luis <luis.almeida.faria@gmail.com>
2025-08-10 21:25:42 -0300

    Add menu access and background for level 1

--- src/consts.py
+++ src/consts.py
@@ -4,9 +4,18 @@ COLOR_BLUE = (0, 128, 255)
 COLOR_GREY = (160, 160, 160)
 COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
+COLOR_YELLOW = (255, 255, 0)
+
+# E
+ENTITY_SPEED = {
+    "Level1Bg0": 0,
+    "Level1Bg1": 1,
+    "Level1Bg2": 2,
+    "Level1Bg3": 3,
+}
 
 # M
-MENU_OPTION = ("NEW GAME 1P", "NEW GAME 2P - COOP", "SCORE", "EXIT")
+MENU_OPTION = ("NEW GAME", "SCORES", "EXIT")
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- src/game.py
+++ src/game.py
@@ -1,6 +1,7 @@
 import pygame
 
-from src.consts import WIN_HEIGHT, WIN_WIDTH
+from src.consts import MENU_OPTION, WIN_HEIGHT, WIN_WIDTH
+from src.level import Level
 from src.menu import Menu
 
 
@@ -12,4 +13,14 @@ class Game:
     def run(self):
         while True:
             menu = Menu(self.window)
-            menu.run()
+            menu_return = menu.run()
+
+            if menu_return == MENU_OPTION[0]:
+                level = Level(self.window, menu_return)
+                level_return = level.run()
+
+            elif menu_return == MENU_OPTION[2]:
+                pygame.quit()
+                quit()
+            else:
+                pass
